0e41c7270c06532c44b174df9305b27a6dd19e8b
Simon Ratner <simon+github@probablyprime.net>
2014-07-14 03:22:46 -0700

    cmd: More robust ssh-agent autorun

--- .cmd
+++ .cmd
@@ -23,9 +23,15 @@ doskey mocha="node_modules/.bin/mocha.cmd" $*
 rem Requires ansicon or a terminal that supports ansi escapes.
 prompt $E[0;1;30m[$D$S$T]$E[0m$S$+$E[0;34m$P$G$E[0m$_$$$S
 
-rem Run ssh-agent with a fixed socket address.
-if not defined SSH_AUTH_SOCK set SSH_AUTH_SOCK=/tmp/ssh-agent
-ssh-agent -a %SSH_AUTH_SOCK% 2>nul
+rem Run ssh-agent, if not already running.
+ps | awk "/ssh-agent/ { print $1 }" > C:\Temp\ssh-agent-pid
+set SSH_AUTH_SOCK=C:\Temp\ssh-agent-sock
+set SSH_AGENT_PID=
+set /p SSH_AGENT_PID=< C:\Temp\ssh-agent-pid
+if not defined SSH_AGENT_PID (
+  rm %SSH_AUTH_SOCK%
+  ssh-agent -a %SSH_AUTH_SOCK%
+)
 
 rem Inject readline command processor.
 "C:\Lib\clink\clink" inject --profile "%USERPROFILE%/.clink"
