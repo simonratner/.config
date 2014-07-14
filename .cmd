f3770bf376266f1b8cd40ea2e9db7bf14a418953
Simon Ratner <simon+github@probablyprime.net>
2014-07-14 03:23:36 -0700

    cmd: Add path to window title

--- .cmd
+++ .cmd
@@ -21,7 +21,7 @@ doskey sails="node_modules/.bin/sails.cmd" $*
 doskey mocha="node_modules/.bin/mocha.cmd" $*
 
 rem Requires ansicon or a terminal that supports ansi escapes.
-prompt $E[0;1;30m[$D$S$T]$E[0m$S$+$E[0;34m$P$G$E[0m$_$$$S
+prompt $E[0;1;30m[$D$S$T]$E[0m$S$+$E[0;34m$P$E]9;3;"$P"$E\$G$E[0m$_$$$S
 
 rem Run ssh-agent, if not already running.
 ps | awk "/ssh-agent/ { print $1 }" > C:\Temp\ssh-agent-pid
