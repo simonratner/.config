c4cf58966c5aafbbb6077c9aa34c8f4bbd09fa81
Simon Ratner <simon+github@probablyprime.net>
2020-07-31 22:20:53 -0700

    Update win tools

--- bin/vimdiff.cmd
+++ bin/vimdiff.cmd
@@ -1,8 +1,14 @@
 @echo off
 
 if exist "%VIMRUNTIME%\vim.exe" goto havevim
-if exist "%ProgramFiles%\Vim\vim74\vim.exe" set VIMRUNTIME=%ProgramFiles%\Vim\vim74
+if exist "%ProgramFiles(x86)%\Vim\vim82\vim.exe" set VIMRUNTIME=%ProgramFiles(x86)%\Vim\vim82
 if exist "%ProgramFiles(x86)%\Vim\vim74\vim.exe" set VIMRUNTIME=%ProgramFiles(x86)%\Vim\vim74
+if exist "%ProgramFiles(x86)%\Vim\vim73\vim.exe" set VIMRUNTIME=%ProgramFiles(x86)%\Vim\vim73
 
 :havevim
-"%VIMRUNTIME%\vim.exe" -d %*
+if exist "%VIMRUNTIME%\vim.exe" (
+  "%VIMRUNTIME%\vim.exe" -d %*
+) else (
+  echo\
+  echo Vim not installed, try https://www.vim.org/download.php#pc
+)
